b3e131cec06a4f2bd37d89dde5ca0df66bd00e84
Ellie Neills <github@esm.me.uk>
2025-02-22 21:44:51 +0000

    Moved code to drawbar() function;

--- osview.c
+++ osview.c
@@ -2,26 +2,30 @@
 /* Copyright (C) 2025, Ellie Neills */
 /* This program is a work in progress */
 
+#include <stdio.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 /* #include <X11/Xos.h> */
 
-#include <stdio.h>
-/* #include <stdlib.h> */
+Display *disp;
+Window win;
+XEvent event;
+GC gc;
+unsigned short win_w, win_h, bar_x, bar_y, bar_w, bar_h, pos, times = 0;
+float us, sy, id, wa, in;
 
 int main(int argc, char *argv[]) {
 
-	Display *disp;
-	Window win;
-	XEvent event;
-	GC gc;
-	XGCValues vals;
-	unsigned short win_w, win_h, bar_x, bar_y, bar_w, bar_h, pos;
-	float us = 0, sy = 0, id = 0;
-	int i, j, c;
 	char *win_name = "x11_osview";
 	XSizeHints *size_hints;
 	XTextProperty title;
+	XGCValues vals;
+	void drawbar(void);
+	unsigned int user, sys, idle, wait, irq, total = 0;
+	unsigned int old_sys, old_user, old_idle, old_wait, old_irq;
+	FILE *fp;
+	int c, i, j;
+	char stats[10][20];
 
 	if(!(disp = XOpenDisplay(0))) {		/* Use $DISPLAY */
 		fprintf(stderr, "Error: Can't connect to X server\n" 
@@ -65,18 +69,48 @@ int main(int argc, char *argv[]) {
 
 	while(1) {
 
-		/* Check for window resize (and other things) */
+		times++;
 
+		fp = fopen("/proc/stat", "r");
+
+		for(i = 0; i < 11; i++){
+			j = 0;
+			while((c = getc(fp)) != ' ') {
+				stats[i][j++] = c;
+			}
+			stats[i][j] = '\0';
+		}
+
+		user = atoi(stats[2]) + atoi(stats[3]);
+		sys = atoi(stats[4]);
+		idle = atoi(stats[5]);
+		wait = atoi(stats[6]);
+		irq = atoi(stats[8]);
+
+		total = (user - old_user) + (sys - old_sys) + (idle - old_idle) +
+			(irq - old_irq) + (wait - old_wait);
+		printf("tot: %d\n", total);
+
+		us = (user - old_user) / (float) total;
+		sy = (sys - old_sys) / (float) total;
+		id = (idle - old_idle) / (float) total;
+		wa = (wait - old_wait) / (float) total;
+		in = (irq - old_irq) / (float) total;
+
+		fclose(fp);
+
+		/* Check for window resize (and other things) */
 		while(XCheckMaskEvent(disp, StructureNotifyMask, &event)) {
 			win_w = event.xconfigure.width;	/* assume window was resized */
 			win_h = event.xconfigure.height;
 			bar_y = win_h * 0.2;
 			bar_w = win_w - 40;
 			bar_h = win_h * 0.7;
-			printf("%dx%d\n", win_w, win_h);
+			/* printf("%dx%d\n", win_w, win_h); */
 			XClearWindow(disp, win);	/* avoids redraw glitch */
 		}
 
+		/* Draw outlines */
 		XDrawRectangle(disp, win, DefaultGC(disp, 0),
 			bar_x, bar_y, bar_w, bar_h);
 		XFillRectangle(disp, win, DefaultGC(disp, 0),
@@ -87,76 +121,49 @@ int main(int argc, char *argv[]) {
 		for(pos = bar_x + 2; pos < (bar_x + bar_w); pos += (bar_w * 0.1)){
 			XDrawLine(disp, win, gc, pos,
 				bar_y + (bar_h * 0.9), pos, bar_y + bar_h);
-		} 
+		}
+
+		if(times > 1)	/* Skip first 2 frames until sane values loaded */
+			drawbar();
+		usleep(500000); /* This is bad practice, but it's simple */
+
+		/* printf("Queue: %d\n", XEventsQueued(disp, win)); */
+
+		/* save previous values */
+		old_sys = sys;
+		old_idle = idle;
+		old_user = user;
+		old_wait = wait;
+		old_irq = irq;
+	}
+}
+
+void drawbar(void) {
 
 		pos = bar_x + 1;	/* Reset position */
 		XSetForeground(disp, gc, 0x0000FF);	/* Blue */
-		XFillRectangle(disp, win, gc, pos, bar_y + 1,
+		XFillRectangle(disp, win, gc, pos, bar_y + 1,	/* user */
 			us * bar_w, (bar_h * 0.9) - 1);
 
 		pos = pos + (us * bar_w);
 		XSetForeground(disp, gc, 0xFF0000);	/* Red */
-		XFillRectangle(disp, win, gc, pos, bar_y + 1,
+		XFillRectangle(disp, win, gc, pos, bar_y + 1,	/* system */
 			sy * bar_w, (bar_h * 0.9) - 1);
 
 		pos = pos + (sy * bar_w);
+		XSetForeground(disp, gc, 0xFFFF00);	/* Yellow */
+		XFillRectangle(disp, win, gc, pos, bar_y + 1,	/* irq */
+			in * bar_w, (bar_h * 0.9) - 1);
+
+		pos = pos + (in * bar_w);
+		XSetForeground(disp, gc, 0x00FFFF);	/* Cyan */
+		XFillRectangle(disp, win, gc, pos, bar_y + 1,	/* iowait */
+			wa * bar_w, (bar_h * 0.9) - 1);
+
+		pos = pos + (wa * bar_w);
 		XSetForeground(disp, gc, 0x00FF00);	/* Green */
-		XFillRectangle(disp, win, gc, pos, bar_y + 1,
+		XFillRectangle(disp, win, gc, pos, bar_y + 1,	/* idle */
 			bar_w - pos + 20, (bar_h * 0.9) - 1);
 
 		XFlush(disp);
-		usleep(50000); /* This is bad practice, but it's simple */
-
-		printf("Queue: %d\n", XEventsQueued(disp, win));
-
-		FILE *fp;
-		fp = fopen("/proc/stat", "r");
-
-		int c, i, j;
-		char stats[10][20];
-		int user, sys, idle, wait, irq;
-		float total = 0;
-		int old_sys, old_user, old_idle;
-		float old_total;
-
-		for(i = 0; i < 11; i++){
-			j = 0;
-			while((c = getc(fp)) != ' ') {
-				stats[i][j++] = c;
-			}
-			stats[i][j] = '\0';
-		}
-		for(i = 0; i < 11; i++)
-			printf("%d: %s\n", i, stats[i]);
-
-		user = atoi(stats[2]) + atoi(stats[3]);
-		sys = atoi(stats[4]);
-		idle = atoi(stats[5]);
-		wait = atoi(stats[6]);
-		irq = atoi(stats[8]);
-		printf("\nuser: %d\n", user - old_user);
-		printf("sys : %d\n", sys - old_sys);
-		printf("idle: %d\n", idle - old_idle);
-		/* printf("wait: %d\n", wait);
-		printf("irq: %d\n", irq); */
-		total = user + sys + idle + wait + irq;
-		/* printf("tot: %f\n", total);
-		printf("old: %f\n", old_total); */
-		printf("dif: %f\n", total - old_total);
-
-		us = (user - old_user) / 448.0;
-		sy = (sys - old_sys) / 448.0;
-		id = (idle - old_idle) / 448.0;
-		printf("us: %f\n", us);
-		printf("sy: %f\n", sy);
-		printf("id: %f\n", id);
-
-		old_total = total;
-		old_sys = sys;
-		old_idle = idle;
-		old_user = user;
-
-		fclose(fp);
-		usleep(1000000);
-	}
 }
